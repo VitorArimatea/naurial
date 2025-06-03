27e04ed7c7f887a26a24b5dabb74474dd17d2b9d
VitorArimatea <vitorarimatea@hotmail.com>
2025-06-03 16:43:22 -0300

    feat: add intl middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware({
+  locales: ["en", "pt", "es"],
+  localePrefix: "always",
+  defaultLocale: "en",
+});
+
+const isPublicRoute = createRouteMatcher(["/", "/pt", "/en", "es"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth.protect();
+  }
+
+  return intlMiddleware(req);
+});
 
 export const config = {
   matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
