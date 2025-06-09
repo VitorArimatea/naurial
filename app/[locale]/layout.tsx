24f3f3b4fb928083fecf7a7ee1bb9f979efe86ba
VitorArimatea <vitorarimatea@hotmail.com>
2025-06-09 11:19:43 -0300

    refactor: improve layout structure

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,56 +1,54 @@
 import type { Metadata } from "next";
+
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "@/components/Navbar";
-
 import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
-import { ClerkProvider } from "@clerk/nextjs";
-
-import { ThemeProvider } from "@/components/theme-provider";
-
-const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
-});
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Naurial",
   description: "Real-time AI Teaching Platform",
 };
 
+const bricolage = Bricolage_Grotesque({
+  variable: "--font-bricolage",
+  subsets: ["latin"],
+});
+
 export default async function LocaleLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }) {
-  const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+  const { locale } = params;
+
+  if (!hasLocale(routing.locales, locale)) notFound();
 
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={`${bricolage.variable} antialiased`}>
         <NextIntlClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+          <ClerkProvider
+            appearance={{ variables: { colorPrimary: "#005ca8" } }}
           >
-            <ClerkProvider
-              appearance={{ variables: { colorPrimary: "#fe5933" } }}
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
             >
-              <Navbar />
+              <Header />
               {children}
-            </ClerkProvider>
-          </ThemeProvider>
+            </ThemeProvider>
+          </ClerkProvider>
         </NextIntlClientProvider>
       </body>
     </html>
